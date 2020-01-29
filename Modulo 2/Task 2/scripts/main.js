a8e49b031e045a31831340fd0496ceb4293e2c12
Luchimonkey <giffedmch@gmail.com>
2020-01-29 17:49:29 -0300

    primer style hecho

--- Modulo 2/Task 2/scripts/main.js
+++ Modulo 2/Task 2/scripts/main.js
@@ -10,5 +10,8 @@ members.forEach(member => {
 	// else{
 	// 	member.middle_name = " " + member.middle_name + " "
 	// }
-	document.getElementById("tablebody").innerHTML += '<tr>' + '<td>' + member.first_name + " " + (member.middle_name || " ") + " " + member.last_name + '</td>' + '<td>' + member.party + '</td>' + '<td>' + member.state + '</td>' + '<td>' + member.seniority + '</td>' + '<td>' + member.votes_with_party_pct + '</td>' + '</tr>'
+	if(member.url==""){
+		member.url="null"
+	}
+	document.getElementById("tablebody").innerHTML += '<tr>' + '<td>' + '<a href="' + member.url + '">' + member.first_name + " " + (member.middle_name || " ") + " " + member.last_name + '</a>' + '</td>' + '<td>' + member.party + '</td>' + '<td>' + member.state + '</td>' + '<td>' + member.seniority + '</td>' + '<td>' + "% " + member.votes_with_party_pct + '</td>' + '</tr>'
 })
