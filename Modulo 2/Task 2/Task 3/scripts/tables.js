368ab60c40b73f4cbc8be971d278433c8c4388fb
Luchimonkey <giffedmch@gmail.com>
2020-02-05 17:30:45 -0300

    task 3 finiquitada punto com

--- Modulo 2/Task 2/Task 3/scripts/tables.js
+++ Modulo 2/Task 2/Task 3/scripts/tables.js
@@ -1,4 +1,4 @@
-const members=data.results[0].members
+const members=data.results[0].members.filter(j => j.total_votes !=0)
 let republicans=0
 let independents=0
 let democrats=0
@@ -6,6 +6,7 @@ let republicansVotes=0
 let independentsVotes=0
 let democratsVotes=0
 
+//-----table1-----
 members.forEach(member =>{
 	if(member.party=="R"){
 		republicans=republicans+1
@@ -38,6 +39,58 @@ members.forEach(member =>{
 	document.getElementById("table1").innerHTML = '<tr>' + '<td>' + 'Democrats' + '</td>' + '<td>' + democrats + '</td>' + '<td>' + democratsPercentage + '%' + '</td>' + '</tr>' + '<tr>' + '<td>' + 'Republicans' + '</td>' + '<td>' + republicans + '</td>' + '<td>' + republicansPercentage + '%' + '</td>' + '</tr>' + '<tr>' + '<td>' + 'Independents' + '</td>' + '<td>' + independents + '</td>' + '<td>' + independentsPercentage + '%' + '</td>' + '</tr>' + '<tr>' + '<td>' + 'Total' + '</td>' + '<td colspan="2">' + total + '</td>' + '</tr>'
 })
 
+
+
+//-----table2-----
+let membersOrdLeast = members.sort(function(a,b){
+	if(a.missed_votes_pct < b.missed_votes_pct){
+		return 1
+	}
+	if(a.missed_votes_pct > b.missed_votes_pct){
+		return -1
+	}
+	return 0
+})
+
+let leastEngaged = []
+let tenPercent1 = (membersOrdLeast.length*0.10).toFixed(0)
+let filterLeast = membersOrdLeast[tenPercent1].missed_votes_pct
+membersOrdLeast.filter(element => element.missed_votes_pct>=filterLeast).forEach(e =>{
+	leastEngaged.push(e)
+})
+leastEngaged.forEach(missed=>{
+	let nombreFull = missed.first_name + " " + (missed.middle_name || " ") + " " + missed.last_name
+	if(document.getElementById("table2")){
+		document.getElementById("table2").innerHTML += `<tr><td>${nombreFull}</td><td>${missed.missed_votes}</td><td>${missed.missed_votes_pct}%</td></tr>`
+	}
+})
+//-----table4-----
+let membersOrdLeastLoyal = members.sort(function (a,b){
+	if(a.votes_with_party_pct > b.votes_with_party_pct){
+		return 1
+	}
+	if(a.votes_with_party_pct < b.votes_with_party_pct){
+		return -1
+	}
+	return 0
+})
+leastLoyal = []
+let filterleastLoyal = membersOrdLeastLoyal[tenPercent1].votes_with_party_pct
+membersOrdLeastLoyal.filter(element => element.votes_with_party_pct<=filterleastLoyal).forEach(e =>{
+	leastLoyal.push(e)
+})
+leastLoyal.forEach(ll =>{
+	let nombreFull = ll.first_name + " " + (ll.middle_name || " ") + " " + ll.last_name
+	let partyVotes = ((ll.votes_with_party_pct/100)*ll.total_votes).toFixed(0)
+	if(document.getElementById("table4")){
+		document.getElementById("table4").innerHTML += `<tr><td>${nombreFull}</td><td>${partyVotes}</td><td>${ll.votes_with_party_pct}%</td></tr>`
+	}
+})
+
+
+
+
+//-----table3-----
 let membersOrd = members.sort(function(a,b){
 	if(a.missed_votes_pct > b.missed_votes_pct){
 		return 1
@@ -48,15 +101,37 @@ let membersOrd = members.sort(function(a,b){
 	return 0
 })
 
-
-let leastEngaged = []
+let mostEngaged = []
 let tenPercent = (membersOrd.length * 0.10).toFixed(0)
 let filter = membersOrd[tenPercent].missed_votes_pct
-membersOrd.filter(element => element.missed_votes_pct<=filter && element.missed_votes_pct != 0).forEach(e =>{
-	leastEngaged.push(e)
+membersOrd.filter(element => element.missed_votes_pct<=filter).forEach(e =>{
+	mostEngaged.push(e)
 })
-console.log(leastEngaged)
-leastEngaged.forEach(missed =>{
+mostEngaged.forEach(missed =>{
 	let nombreFull = missed.first_name + " " + (missed.middle_name || " ") + " " + missed.last_name
-	document.getElementById("table2").innerHTML += `<tr><td>${nombreFull}</td><td>${missed.missed_votes}</td><td>${missed.missed_votes_pct}%</td></tr>`
+	if(document.getElementById("table3")){
+		document.getElementById("table3").innerHTML += `<tr><td>${nombreFull}</td><td>${missed.missed_votes}</td><td>${missed.missed_votes_pct}%</td></tr>`
+	}
+})
+//-----table5-----
+let membersOrdMostLoyal = members.sort(function (a,b){
+	if(a.votes_with_party_pct < b.votes_with_party_pct){
+		return 1
+	}
+	if(a.votes_with_party_pct > b.votes_with_party_pct){
+		return -1
+	}
+	return 0
+})
+mostLoyal = []
+let filterMostLoyal = membersOrdMostLoyal[tenPercent].votes_with_party_pct
+membersOrdMostLoyal.filter(element => element.votes_with_party_pct>=filterMostLoyal).forEach(e =>{
+	mostLoyal.push(e)
+})
+mostLoyal.forEach(ll =>{
+	let nombreFull = ll.first_name + " " + (ll.middle_name || " ") + " " + ll.last_name
+	let partyVotes = ((ll.votes_with_party_pct/100)*ll.total_votes).toFixed(0)
+	if(document.getElementById("table5")){
+		document.getElementById("table5").innerHTML += `<tr><td>${nombreFull}</td><td>${partyVotes}</td><td>${ll.votes_with_party_pct}%</td></tr>`
+	}
 })
